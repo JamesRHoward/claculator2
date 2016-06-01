6b2e79c716b282b6d6d8dd2f9361576e04258e00
Alan Denison and James Howard <james@>
2016-06-01 14:43:12 -0700

    used jsQuery to add -,*,and / to the calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,52 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function() {
+  $("button#addbtn").click(function(event) {
+  event.preventDefault()
+  var number1 = parseInt($("#imput1").val());
+  var number2 = parseInt($("#imput2").val());
+  var result = add(number1, number2);
+  $("#output").text(result);
+  });
+});
 
-alert(add(number1, number2));
+$(document).ready(function() {
+  $("button#subtractbtn").click(function(event) {
+  event.preventDefault()
+  var number1 = parseInt($("#imput1").val());
+  var number2 = parseInt($("#imput2").val());
+  var result = subtract(number1, number2);
+  $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("button#multiplybtn").click(function(event) {
+  event.preventDefault()
+  var number1 = parseInt($("#imput1").val());
+  var number2 = parseInt($("#imput2").val());
+  var result = multiply(number1, number2);
+  $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("button#dividebtn").click(function(event) {
+  event.preventDefault()
+  var number1 = parseInt($("#imput1").val());
+  var number2 = parseInt($("#imput2").val());
+  var result = divide(number1, number2);
+  $("#output").text(result);
+  });
+});
